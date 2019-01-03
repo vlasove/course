916a73659fb564b3d8161253892c04f8419dfdcc
PythonCode <PythonCode>
2019-01-04 01:01:07 +0300

    Commit 1

--- whilewhile.cpp
+++ whilewhile.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    
+    int n;
+    cin >> n;
+    int sum = 0;
+    if (n >0) {
+        while (n> 0){
+            sum += n;
+            --n;
+
+        }
+        cout << sum<<endl;
+
+    }else{
+        cout<<"Enter Positive Number"<<endl;
+    }
     return 0;
 }
