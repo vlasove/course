83a716788d32458b57eef2d61f4eb83205c753aa
PythonCode <PythonCode>
2019-01-04 02:42:56 +0300

    h

--- ProgrAssesm7.cpp
+++ ProgrAssesm7.cpp
@@ -6,6 +6,18 @@ int main(int argc, char const *argv[])
     int a,b;
     cin >>a>>b;
     int minimal_num;
-    
+    if (a >= b){
+        minimal_num = b;
+    }else{
+        minimal_num = a;
+    }
+
+    for (int i = minimal_num; i > 0; i--){
+        if(a%i ==0 && b%i ==0){
+            cout<<i;
+            break;
+        }
+    }
+
     return 0;
 }
