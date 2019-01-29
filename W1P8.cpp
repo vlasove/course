7b6cad575c742d5eae2db4989f91040d44dddff1
Evgen <evgeny_vlasov@yahoo.com>
2019-01-30 00:36:02 +0300

    Return Tuple Files

--- W1P8.cpp
+++ W1P8.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <utility>
 
 using namespace std;
 
 int main(){
 
     tuple<int, string, bool> t(7, "C++", true);
+
+    auto q = make_tuple(8, "Rust", false);
+
+    cout<<get<0>(q)<<get<1>(q)<<get<2>(q)<<endl;
+
+    pair<int, string> p(10, "Python");
+
+    cout<<p.first <<" and "<<p.second<<endl;
+
 
 
 
---
+++ W1P9.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <tuple>
+#include <map>
+#include <set>
+
+
+
+using namespace std;
+
+class Cities{
+    public:
+        tuple<bool, string> FindCountry(const string& city) const {
+            if(city_to_country.count(city) ==1){
+                return make_tuple(true, city_to_country.at(city));
+            }else if (ambig_cities.count(city) == 1){
+                return make_tuple(false, "Ambiguous");
+            }else{
+                return make_tuple(false, "Not Exists");
+            }
+
+        }
+
+    private:
+        map<string, string> city_to_country;
+        set<string> ambig_cities;
+};
+
+int main(){
+
+    Cities cities;
+    auto t = cities.FindCountry("Moscow");
+    cout << get<0>(t)<< get<1>(t)<<endl;
+
+
+    return 0;
+}
