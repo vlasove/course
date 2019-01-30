118fffdd1f3b7c7d08df0feb1179e58fd8c02064
Evgen <evgeny_vlasov@yahoo.com>
2019-01-30 11:43:10 +0300

    First Steps in task tracker

---
+++ TaskManager.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <map>
+
+using namespace std;
+// Перечислимый тип для статуса задачи
+enum class TaskStatus {
+  NEW,          // новая
+  IN_PROGRESS,  // в разработке
+  TESTING,      // на тестировании
+  DONE          // завершена
+};
+
+// Объявляем тип-синоним для map<TaskStatus, int>,
+// позволяющего хранить количество задач каждого статуса
+using TasksInfo = map<TaskStatus, int>;
+
+class TeamTasks {
+public:
+  // Получить статистику по статусам задач конкретного разработчика
+  const TasksInfo& GetPersonTasksInfo(const string& person) const;
+  
+  // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
+  void AddNewTask(const string& person);
+  
+  // Обновить статусы по данному количеству задач конкретного разработчика,
+  // подробности см. ниже
+  tuple<TasksInfo, TasksInfo> PerformPersonTasks(
+      const string& person, int task_count);
+
+private:
+    map<string, TasksInfo> persons;
+};
+
+
+
+void PrintTasksInfo(TasksInfo tasks_info) {
+  cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
+      ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
+      ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
+      ", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
+}
+
+int main(){
+
+    return 0;
+}
