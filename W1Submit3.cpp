3acda34d5813ab74d21557802eddb78b58f8acca
Evgen <evgeny_vlasov@yahoo.com>
2019-01-31 02:17:39 +0300

    Ultra Hard Tracker Completed

--- W1Submit3.cpp
+++ W1Submit3.cpp
@@ -1,21 +1,3 @@
-#include <iostream>
-#include <tuple>
-#include <vector>
-#include <map>
-
-using namespace std;
-// Перечислимый тип для статуса задачи
-enum class TaskStatus {
-  NEW,          // новая
-  IN_PROGRESS,  // в разработке
-  TESTING,      // на тестировании
-  DONE          // завершена
-};
-
-// Объявляем тип-синоним для map<TaskStatus, int>,
-// позволяющего хранить количество задач каждого статуса
-using TasksInfo = map<TaskStatus, int>;
-
 class TeamTasks {
 public:
   // Получить статистику по статусам задач конкретного разработчика
@@ -49,10 +31,8 @@ public:
          }
 
         untouched.erase(TaskStatus::DONE);
-        TasksInfo to_erase;
-        
 
-            for (size_t i=0; i<3; i++){   //листаем массив TaskInfo по ключам 
+                 for (size_t i=0; i<3; i++){   //листаем массив TaskInfo по ключам 
             if (!task_count)          //TaskStatus используя числовые
                 break;                //значения этих ключей
                 
@@ -63,8 +43,7 @@ public:
               
                 updated [next_status]=untouched[status];
                 task_count-=untouched[status];
-                to_erase[status] = untouched[status];
-                //untouched.erase(status);
+                untouched.erase(status);
 
                 persons[person][status] = 0;
                 persons[person][next_status]+=updated [next_status];
@@ -80,19 +59,10 @@ public:
                 break;
             }
          }
-
-         TasksInfo new_untouched;
-         for(size_t i  = 0; i < 3; i++){
-             TaskStatus check_status=static_cast<TaskStatus>(i);
-
-             if(to_erase.count(check_status) == 0){
-                new_untouched[check_status] = untouched[check_status];
-             }
-         }
-         
-         return tie (updated, new_untouched);
+         return tie (updated, untouched);
 
 
+        return tie(updated, untouched);
 
           
 
@@ -101,43 +71,3 @@ public:
 private:
     map<string, TasksInfo> persons;
 };
-
-
-
-void PrintTasksInfo(TasksInfo tasks_info) {
-  cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
-      ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
-      ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
-      ", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
-}
-
-int main(){
-    TeamTasks tasks;
-    tasks.AddNewTask("Ilia");
-    for (int i = 0; i < 3; ++i) {
-        tasks.AddNewTask("Ivan");
-    }
-    cout << "Ilia's tasks: ";
-    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
-    cout << "Ivan's tasks: ";
-    PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
-
-    TasksInfo updated_tasks, untouched_tasks;
-  
-    tie(updated_tasks, untouched_tasks) =
-        tasks.PerformPersonTasks("Ivan", 2);
-    cout << "Updated Ivan's tasks: ";
-    PrintTasksInfo(updated_tasks);
-    cout << "Untouched Ivan's tasks: ";
-    PrintTasksInfo(untouched_tasks);
-    
-    tie(updated_tasks, untouched_tasks) =
-        tasks.PerformPersonTasks("Ivan", 2);
-    cout << "Updated Ivan's tasks: ";
-    PrintTasksInfo(updated_tasks);
-    cout << "Untouched Ivan's tasks: ";
-    PrintTasksInfo(untouched_tasks);
-
-
-    return 0;
-}
--- W1Submit3_1.cpp
+++ W1Submit3_1.cpp
@@ -1,21 +1,3 @@
-#include <iostream>
-#include <tuple>
-#include <vector>
-#include <map>
-
-using namespace std;
-// Перечислимый тип для статуса задачи
-enum class TaskStatus {
-  NEW,          // новая
-  IN_PROGRESS,  // в разработке
-  TESTING,      // на тестировании
-  DONE          // завершена
-};
-
-// Объявляем тип-синоним для map<TaskStatus, int>,
-// позволяющего хранить количество задач каждого статуса
-using TasksInfo = map<TaskStatus, int>;
-
 class TeamTasks {
 public:
   // Получить статистику по статусам задач конкретного разработчика
@@ -101,43 +83,3 @@ public:
 private:
     map<string, TasksInfo> persons;
 };
-
-
-
-void PrintTasksInfo(TasksInfo tasks_info) {
-  cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
-      ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
-      ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
-      ", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
-}
-
-int main(){
-    TeamTasks tasks;
-    tasks.AddNewTask("Ilia");
-    for (int i = 0; i < 3; ++i) {
-        tasks.AddNewTask("Ivan");
-    }
-    cout << "Ilia's tasks: ";
-    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
-    cout << "Ivan's tasks: ";
-    PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
-
-    TasksInfo updated_tasks, untouched_tasks;
-  
-    tie(updated_tasks, untouched_tasks) =
-        tasks.PerformPersonTasks("Ivan", 2);
-    cout << "Updated Ivan's tasks: ";
-    PrintTasksInfo(updated_tasks);
-    cout << "Untouched Ivan's tasks: ";
-    PrintTasksInfo(untouched_tasks);
-    
-    tie(updated_tasks, untouched_tasks) =
-        tasks.PerformPersonTasks("Ivan", 2);
-    cout << "Updated Ivan's tasks: ";
-    PrintTasksInfo(updated_tasks);
-    cout << "Untouched Ivan's tasks: ";
-    PrintTasksInfo(untouched_tasks);
-
-
-    return 0;
-}
