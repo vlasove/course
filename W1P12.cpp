4129b6944f38e5b7f20ff9eee4c46120f46b0366
Evgen <evgeny_vlasov@yahoo.com>
2019-02-08 16:11:57 +0300

    Second Commit

--- W1P12.cpp
+++ W1P12.cpp
@@ -7,7 +7,17 @@
 using namespace std;
 
 
-ostream& operator<<(ostream& out, const vector<int>& v){
+
+template <typename First, typename Second>
+ostream& operator<<(ostream& out, const pair<First, Second>& p ){
+    out<<p.first<<','<<p.second;
+    return out;
+
+}
+
+
+template <typename T>
+ostream& operator<<(ostream& out, const vector<T>& v){
     for (const auto i : v){
         out << i<<' ';
     }
@@ -16,10 +26,24 @@ ostream& operator<<(ostream& out, const vector<int>& v){
 }
 
 
+template <typename Key, typename Value>
+ostream& operator<<(ostream& out, const map<Key, Value>& m){
+    for (const auto i : m){
+        out << i<<' ';
+    }
+    return out;
+
+}
+
+
+
 
 int main(){
     vector<int> v = {1,2,3};
     cout << v <<endl;
+
+    map<int, int> m = {{1,2}, {3,4}};
+    cout << m<< endl;
 
 
 
