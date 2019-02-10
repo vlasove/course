fb58e3dc58af4914659b7cb4d336fbb7326e21a7
Evgen <evgeny_vlasov@yahoo.com>
2019-02-10 20:45:57 +0300

    First Part of unit Test framework

---
+++ W2P3.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <cassert>
+#include <exception>
+#include <sstream>
+
+using namespace std;
+using Synonyms = map<string, set<string>>;
+
+void AddSynonyms(map<string, set<string>>& synonyms, const string& fw, const string& sw){
+            synonyms[sw].insert(fw);
+            synonyms[fw].insert(sw);
+
+}
+
+size_t GetSynonymsCount(map<string, set<string>>& synonyms, const string& fw){
+    return synonyms[fw].size();
+}
+
+bool SynonymsCheck(map<string, set<string>>& synonyms, const string& fw, const string& sw){
+     if(synonyms[fw].count(sw) == 1){
+                return true;
+            }else{
+                return false;
+            }
+
+}
+template <class Q>
+ostream& operator << (ostream& os, const set<Q>& s){
+    os<<"{";
+    bool first = true;
+    for(const auto& elem :s){
+        if(!first){
+            os<<",";
+        }
+        first = false;
+        os<<elem;
+    }
+    return os<<" }";
+
+}
+
+template<class K, class V>
+ostream& operator<< (ostream& os, const map<K,V>& m){
+    os <<"{";
+    bool first = true;
+    for(const auto& kv :m){
+        if(!first){
+            os<<", ";
+        }
+        first = false;
+        os<<kv.first <<": "<<kv.second;
+    }
+    return os<<"}";
+}
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint){
+    if(t != u){
+        ostringstream os;
+        os <<"Asserion failed : "<< t << " != "<<u<<" Hint : "<<hint;;
+        throw runtime_error(os.str());
+    }
+}
+
+void Assert(bool b, const string& hint){
+    AssertEqual(b,true, hint);
+}
+
+void TestAddSynonyms(){
+
+    {
+        Synonyms synonyms;
+        AddSynonyms(synonyms, "a","b");
+        const Synonyms expected = {
+            {"a",{"b"}},
+            {"b",{"a"}}
+            };
+        AssertEqual(synonyms , expected, "expected assert");
+
+    }
+
+    {
+        Synonyms synonyms = {
+            {"a", {"b"}},
+            {"b",{"a","c"}},
+            {"c", {"b"}}
+        };
+        AddSynonyms(synonyms, "a","c");
+        const Synonyms expected ={
+            {"a", {"b","c"}},
+            {"b",{"a","c"}},
+            {"c", {"b","a"}}
+        };
+        assert(synonyms == expected);
+    }
+    cout<<"AddSynonymsTest is OK!"<<endl;
+}
+
+void TestCount(){
+    {
+        Synonyms synonyms;
+        AssertEqual(GetSynonymsCount(synonyms, "a") , 0u, "empty");
+    }
+    {
+         Synonyms synonyms ={
+            {"a", {"b","c"}},
+            {"b",{"a","c"}},
+            {"c", {"b","a"}}
+        };
+        AssertEqual(GetSynonymsCount(synonyms, "a"), 2u, "a");
+        AssertEqual(GetSynonymsCount(synonyms, "b"), 2u, "b");
+        AssertEqual(GetSynonymsCount(synonyms, "d"), 0u, "c");
+    }
+
+    cout<<"GetSynonymsCountTest is OK!"<<endl; 
+}
+
+void TestSynonymsCheck(){
+    {
+        Synonyms synonyms;
+        Assert(!SynonymsCheck(synonyms, "a","b"), "lol");
+        Assert(!SynonymsCheck(synonyms, "b","a"), "kek");
+    }
+
+    cout<<"SynonymsCheckTest is OK!"<<endl;
+}
+
+template<class TestFunc>
+void RunTest(TestFunc func, const string& test_name){
+    try{
+        func();
+    } catch(runtime_error& r){
+        cout<<test_name<<" Failed: "<<r.what()<<endl;
+    }
+
+}
+
+void TestAll(){
+    RunTest(TestCount, "TestCount");
+    RunTest(TestSynonymsCheck, "TestSynonymsCheck");
+    RunTest(TestAddSynonyms, "TestAddSynonyms");
+
+}
+
+
+int main(){
+    TestAll();
+
+    return 0;
+    int q;
+    cin >> q;
+
+    map<string, set<string>> synonyms;
+    for (int i =0; i < q; ++i){
+        string operation_code;
+        cin >> operation_code;
+
+        if(operation_code == "ADD"){
+            string first_word, second_word;
+            cin >>first_word >> second_word;
+            AddSynonyms(synonyms, first_word, second_word);
+ 
+
+
+        }else if (operation_code =="COUNT"){
+            string word;
+            cin >> word;
+            cout << GetSynonymsCount(synonyms, word)<<endl;
+        } else if(operation_code == "CHECK"){
+            string first_word, second_word;
+            cin >>first_word >> second_word;
+            if(SynonymsCheck(synonyms, first_word, second_word)){
+                cout<<"YES"<<endl;
+            }else{
+                cout<<"NO"<<endl;
+            }
+           
+        }
+    }
+
+
+
+    return 0;
+}
